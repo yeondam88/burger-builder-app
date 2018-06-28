735c03775d1e8d4c99b168935a28f01add480d2f
Yeondam Park <giyen9088@gmail.com>
2018-06-27 21:29:06 -0700

    adding components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+
 class App extends Component {
   render() {
-    return <div>My Burger</div>;
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
 }
 
