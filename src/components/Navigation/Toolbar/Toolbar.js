695323d3579dd46cd1ec80df62039abb058cf078
Yeondam Park <giyen9088@gmail.com>
2018-07-01 21:33:12 -0700

    give style on toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import classes from "./Toolbar.css";
+
 const toolbar = props => (
-  <header>
+  <header className={classes.Toolbar}>
     <div>MENU</div>
     <div>LOGO</div>
     <nav>...</nav>
