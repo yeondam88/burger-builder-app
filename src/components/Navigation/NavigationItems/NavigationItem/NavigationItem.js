34103a80083c314ebcbca8a3be22cb268a4e41bc
Yeondam Park <giyen9088@gmail.com>
2018-07-04 22:58:36 -0700

    adding navigation with styled

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+import classes from "./NavigationItem.css";
+
 const navigationItem = props => (
-  <li>
-    <a href="/">A Link</a>
+  <li className={classes.NavigationItem}>
+    <a href={props.link} className={props.active ? classes.active : null}>
+      {props.children}
+    </a>
   </li>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-const navigationItems = () => <ul />;
+import classes from "./NavigationItems.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const navigationItems = () => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" active>
+      Burger Builder
+    </NavigationItem>
+    <NavigationItem link="/">Checkout</NavigationItem>
+  </ul>
+);
 
 export default navigationItems;
