34b226563eabee25c296c735d7ba6528e4984d02
Yeondam Park <giyen9088@gmail.com>
2018-07-05 22:22:14 -0700

    adding a sidedrawer menu toggle button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,7 +7,7 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   };
 
   sideDrawerClosedHandler = () => {
@@ -16,10 +16,16 @@ class Layout extends Component {
     });
   };
 
+  sideDrawerOpenHandler = () => {
+    this.setState({
+      showSideDrawer: true
+    });
+  };
+
   render() {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar openMenu={this.sideDrawerOpenHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,7 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <div onClick={props.openMenu}>MENU</div>
     <div className={classes.Logo}>
       <Logo />
     </div>
