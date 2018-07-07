6215defb0c029464a7fdd3a16c2b182089b5067c
Yeondam Park <giyen9088@gmail.com>
2018-07-06 22:00:33 -0700

    quick fixes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -16,16 +16,18 @@ class Layout extends Component {
     });
   };
 
-  sideDrawerOpenHandler = () => {
-    this.setState({
-      showSideDrawer: true
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return {
+        showSideDrawer: !prevState.showSideDrawer
+      };
     });
   };
 
   render() {
     return (
       <Aux>
-        <Toolbar openMenu={this.sideDrawerOpenHandler} />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const drawerToggle = props => (
+  <div onClick={props.drawerToggleClicked}>Menu</div>
+);
+
+export default drawerToggle;
